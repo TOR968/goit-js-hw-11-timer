863b950c68e6b3822c324b6708d035fc2f96635a
Maxim Turbal <tor968@yahoo.com>
2020-10-25 16:10:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,50 @@
-new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
-});
+class CountdownTimer {
+  constructor(selector, targetDate) {
+    this.selector = document.querySelector(selector);
+    this.targetDate = new Date(targetDate);
+  }
 
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  startCountdown() {
+    this.intervalId = setInterval(() => {
+      this.currentTime = new Date();
+      this.remainingTime = this.targetDate - this.currentTime;
+      this.timeCounter(), this.insertTimeIntoMarckup();
+      if (this.remainingTime < 0) {
+        this.stopCountdown();
+      }
+    }, 1000);
+  }
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  timeCounter() {
+    this.days = Math.floor(this.remainingTime / (1000 * 60 * 60 * 24));
+    this.hours = Math.floor((this.remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    this.mins = Math.floor((this.remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+    this.secs = Math.floor((this.remainingTime % (1000 * 60)) / 1000);
+  }
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  insertTimeIntoMarckup() {
+    this.refs = {
+      days: document.querySelector(" [data-value=days]"),
+      hours: document.querySelector(" [data-value=hours]"),
+      minutes: document.querySelector(" [data-value=mins]"),
+      seconds: document.querySelector(" [data-value=secs]")
+    };
+    this.refs.days.textContent = this.pad(this.days);
+    this.refs.hours.textContent = this.pad(this.hours);
+    this.refs.minutes.textContent = this.pad(this.mins);
+    this.refs.seconds.textContent = this.pad(this.secs);
+  }
+  stopCountdown() {
+    clearInterval(this.intervalId);
+    this.remainingTime = 0;
+    this.timeCounter(this.remainingTime);
+    this.insertTimeIntoMarckup(this.remainingTime);
+  }
 
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+}
+
+const countdownTimer = new CountdownTimer("#timer-1", "October 25, 2020, 16:01");
+countdownTimer.startCountdown();
